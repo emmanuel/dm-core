9071826c30153d71b89178ecabe4c0930d142b09
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-06-11 10:07:12 -0700

    Reformat a block in Query#self_relationship.

--- lib/dm-core/query.rb
+++ lib/dm-core/query.rb
@@ -1463,16 +1463,15 @@ module DataMapper
     #
     # @api private
     def self_relationship
-      @self_relationship ||=
-        begin
-          model = self.model
-          Associations::OneToMany::Relationship.new(
-            :self,
-            model,
-            model,
-            self_relationship_options
-          )
-        end
+      @self_relationship ||= begin
+        model = self.model
+        Associations::OneToMany::Relationship.new(
+          :self,
+          model,
+          model,
+          self_relationship_options
+        )
+      end
     end
 
     # Return options for the self referrential relationship
